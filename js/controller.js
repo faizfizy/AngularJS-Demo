f785b7332e4b5614c6075b2d72ea250d6ff9693e
faizfizy <faizfizy@gmail.com>
2016-12-07 16:55:34 +0800

    Advance filter using orderBy & subfilter(direction)

--- js/controller.js
+++ js/controller.js
@@ -4,6 +4,7 @@ myApp.controller('MyController', ['$scope', '$http', function ($scope, $http) {
 
     $http.get('js/data.json').then(function (response) {
         $scope.hero = response.data;
+        $scope.order = "nickname";
     });
 
 }]);
