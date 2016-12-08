cc58f0b3724cf100c7d42c4ea8608094caabce12
faizfizy <faizfizy@gmail.com>
2016-12-08 11:55:41 +0800

    add new partials and route for it

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+/*global angular */
 var myApp = angular.module('myApp', ['ngRoute', 'heroControllers']);
 
-myApp.config(['$routeProvider', function($routeProvider) {
+myApp.config(['$routeProvider', function ($routeProvider) {
+    "use strict";
     $routeProvider.
     when('/list', {
         templateUrl: 'partials/list.html',
         controller: 'ListController'
+    }).
+    when('/details/:itemId', {
+        templateUrl: 'partials/details.html',
+        controller: 'DetailsController'
     }).
     otherwise({
         redirectTo: '/list'
--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,18 @@
+/*global angular */
 var heroControllers = angular.module('heroControllers', []);
 
 heroControllers.controller('ListController', ['$scope', '$http', function ($scope, $http) {
+    "use strict";
     $http.get('js/data.json').then(function (response) {
         $scope.hero = response.data;
         $scope.order = "nickname";
+    });
+}]);
+
+heroControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    "use strict";
+    $http.get('js/data.json').then(function (response) {
+        $scope.hero = response.data;
+        $scope.whichItem = $routeParams.itemId;
     });
 }]);
